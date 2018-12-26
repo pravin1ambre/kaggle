3bad863a1799ed71eaa2811232e81df41e23464c
pravin <pambre.ht@gmail.com>
2018-12-25 20:28:53 -0500

    developer branche

---
+++ send.py
@@ -0,0 +1,30 @@
+import pika, os
+
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello CloudAMQP!')
+
+print(" [x] Sent 'Hello World!'")
+connection.close()
+
+
+
+# #!/usr/bin/env python
+# import pika
+
+# connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
+
+
+# channel.queue_declare(queue='hello')
+
+# channel.basic_publish(exchange='',
+#                       routing_key='hello',
+#                       body='Hello World!')
+# print(" [x] Sent 'Hello World!'")
+# connection.close()
